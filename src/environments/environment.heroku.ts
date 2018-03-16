b1d0d52f558d148c20554771ecd6d0587f1e1975
Bertrand Bougon <bertrand.bougon@gmail.com>
2018-03-16 22:28:58 +0100

    remove heroku files

--- src/environments/environment.heroku.ts
+++
@@ -1,7 +0,0 @@
-const API_URL = 'https://ou-sont-mes-affaires-api.herokuapp.com';
-
-export const environment = {
-  production: true,
-  envName: 'heroku',
-  locationResource: API_URL + '/locations',
-};
