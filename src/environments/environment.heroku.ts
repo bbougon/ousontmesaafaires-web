d76e8de711eb3d27ec58d3e80462af49edb12204
Bertrand Bougon <bertrand.bougon@gmail.com>
2018-03-16 21:14:11 +0100

    Add heroku environment

--- src/environments/environment.heroku.ts
+++ src/environments/environment.heroku.ts
@@ -2,5 +2,6 @@ const API_URL = 'https://ou-sont-mes-affaires-api.herokuapp.com';
 
 export const environment = {
   production: true,
+  envName: 'heroku',
   locationResource: API_URL + '/locations',
 };
