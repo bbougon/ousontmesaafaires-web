9eb70e70ed9b5a58dc636b1763f3e0d4bf932ca7
Bertrand Bougon <bertrand.bougon@gmail.com>
2018-05-03 00:22:02 +0200

    Fix date time provider test with utc date

--- src/app/infrastructure/date-time-provider.spec.ts
+++ src/app/infrastructure/date-time-provider.spec.ts
@@ -4,10 +4,10 @@ describe('DateTimeProvider', () => {
 
   it('should give the unix timestamp', () => {
     const date = new Date();
-    date.setFullYear(2018, 1, 10);
-    date.setHours(10, 15, 12, 267);
+    date.setUTCFullYear(2018, 1, 10);
+    date.setUTCHours(10, 15, 12, 267);
     const dateTimeProvider = new DateTimeProvider(date);
 
-    expect(dateTimeProvider.unixTimestamp()).toBe(1518254112);
+    expect(dateTimeProvider.unixTimestamp()).toBe(1518257712);
   });
 });
